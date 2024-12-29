e1854ad09504436ab1f9f595812bb55bdf019dc8
FroggyCoder <alaxwarden@gmail.com>
2024-12-28 22:27:59 -0700

    Update collatz.py

--- collatz/collatz.py
+++ collatz/collatz.py
@@ -1,17 +1,13 @@
 class collatz:
   num: int
   F_stop: bool
-  def __init__(s, num: int)->None: 
+  F_fastCalc: bool
+  def __init__(s, num: int, fastCalc: bool = False)->None: 
     s.F_stop = False
-    s.num=num
+    s.num = num
+    s.F_fastCalc = fastCalc
   def calc(s)->int:
-    if s.num % 2 != 0: s.num=s.num*3+1 
-    else: s.num /= 2
-    return int(s.num)
-  def calc(s)->int: # Faster method
-    # This line is here so if the number will be even after another devision, we can conclude it will divide forever till 1
-    # This means ending early will not effect the result!
-    if s.num % 4 == 0: s.F_stop = True; return -0
+    if s.num % 4 == 0 and s.F_fastCalc: s.F_stop = True; return -0
     elif s.num % 2 != 0: s.num=s.num*3+1 
     else: s.num /= 2
     return int(s.num)
@@ -35,23 +31,27 @@ class collatz:
     if currentTest == ouputNums: return 1
     else: return 2
 
-def debugMain_()->int:
+def debugMain_(fastCalc: bool = False)->int:
   debugNum_: int = int(input("Starting number: "))
-  debugC_ = collatz(debugNum_)
+  debugC_ = collatz(debugNum_, fastCalc)
   return debugC_.testCalc_()
 
-def main(debugMode: bool)->int:
-  if debugMode: return debugMain_()
+def main(debugMode: bool, fastCalc: bool = False)->int:
+  if debugMode: return debugMain_(fastCalc)
   new: int
 
   num: int = int(input("Starting number: "))
-  c = collatz(num)
+  c = collatz(num, fastCalc)
   while c.num != 1 and not c.F_stop:
     new = c.calc()
     print(f"out: {num} | {new}")
   return -1
 
 mode: str
+fastCalc: str
+mainArgs: list
 while True:
-  mode = input("Debug mode y/n: ").lower()
-  if input( f"main exited with code {main(mode == "y")}... enter to exit > " ) != ".ag": break
+  mode     = input("Debug mode y/n: ").lower()
+  fastCalc = input("fastCalc mode y/n: ").lower()
+  mainArgs = [mode == "y", fastCalc == "y"]
+  if input( f"main exited with code {main(*mainArgs)}... enter to exit > " ) != ".ag": break
